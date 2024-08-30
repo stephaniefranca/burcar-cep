1c6efbb186f6545c41a731f79b8a9fa42cf72051
Stephanie de França <stephanie.dfranca@gmail.com>
2024-08-30 19:35:13 -0300

    primeiro commit

--- src/Principal.java
+++ src/Principal.java
@@ -1,5 +1,24 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args){
+
+        Scanner leitura = new Scanner(System.in);
+        ConsultaCep consultaCep = new ConsultaCep();
+        System.out.println("Digite um numero de CEP para consultar: ");
+        var cep = leitura.nextLine();
+
+        try {
+            Endereco novoEndereco  =  consultaCep.buscaEndereco(cep);
+            System.out.println(novoEndereco);
+            GeradorDeArquivo gerador = new GeradorDeArquivo();
+            gerador.salvaJson(novoEndereco);
+
+        } catch (RuntimeException | IOException | InterruptedException e){
+            System.out.println(e.getMessage());
+            System.out.println("Finalizando a aplicação.");
+        }
+
     }
 }
